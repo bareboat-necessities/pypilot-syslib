5acb0b40367a30a9442ee952574def8bea1f6c2a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-18 15:49:03 -0400

    Use stdout log sink in Linux example

--- examples/linux/logging_example.cpp
+++ examples/linux/logging_example.cpp
@@ -1,18 +1,11 @@
 #include <pypilot_syslib.hpp>
 
-class CountSink : public pypilot_syslib::LogSink {
-public:
-    CountSink() : count(0) {}
-    void write(const pypilot_syslib::LogRecord&) override { ++count; }
-    int count;
-};
-
 int main() {
-    CountSink sink;
+    pypilot_syslib::StdoutLogSink sink;
     pypilot_syslib::Logger logger(&sink);
     logger.info(1000ULL,
                 pypilot_syslib::LogModule::Runtime,
                 pypilot_syslib::LogEvent::SystemStartup,
                 "start");
-    return sink.count == 1 ? 0 : 1;
+    return 0;
 }
